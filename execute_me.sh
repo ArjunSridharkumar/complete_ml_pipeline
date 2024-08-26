1e7a00eeb1570c9dee7cc800924dc88dde1eff7a
ArjunSridharkumar <98350094+ArjunSridharkumar@users.noreply.github.com>
2024-08-26 08:07:36 +0000

    automation scripts

---
+++ execute_me.sh
@@ -0,0 +1,10 @@
+#Steps to be executed
+
+#Pull the docker image
+docker pull arjunsridhar9720/rbc_assessment:latest
+
+#Run the airflow server and webserc
+docker run -d -p 8080:8080 -p 5000:5000 --name ml_pipeline_container arjunsridhar9720/rbc_assessment:latest
+docker exec ml_pipeline_container bash /opt/airflow/scripts/start_airflow_server.sh
+
+#Run
