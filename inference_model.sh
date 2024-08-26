450c634d6cfbe4eae1f730dac170fc8a7ad203f6
ArjunSridharkumar <98350094+ArjunSridharkumar@users.noreply.github.com>
2024-08-26 11:25:29 +0000

    updates

--- inference_model.sh
+++ inference_model.sh
@@ -5,6 +5,6 @@
 
 #Run the airflow server and webserc
 # docker run -d -p 8080:8080 -p 5000:5000 --name ml_pipeline_container arjunsridhar9720/rbc_assessment:latest
-docker exec ml_pipeline_container bash /opt/airflow/dags/scripts/start_airflow_server.sh
+docker exec ml_pipeline_container bash /opt/airflow/dags/scripts/start_flask_app.sh
 
 #Run
